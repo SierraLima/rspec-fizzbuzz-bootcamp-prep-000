d488e40567690f3fe169c19cf5494bd13cff9402
Stéphane <stephluethi@hotmail.com>
2018-06-18 20:39:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0
-   "Fizz"
-  end
-  if int % 5 == 0
-   "Buzz"
-  end
+  
   if int % 3 == 0 && int % 5 == 0 
    "FizzBuzz"
+  elsif int % 3 == 0
+   "Fizz"
+  
+  elsif int % 5 == 0
+   "Buzz"
+  else
+  
   end 
   
 end
